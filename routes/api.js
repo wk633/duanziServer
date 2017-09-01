918a00d86f3dbeded50bc76862069c1be7846136
wk633 <wk633@outlook.com>
2017-08-31 23:16:18 -0700

    update to random access

--- routes/api.js
+++ routes/api.js
@@ -3,17 +3,18 @@ const router = express.Router();
 const jandan = require('mongoose').model('jandan');
 
 /* GET users listing. */
-router.get('/duanzi/:offset', function(req, res, next) {
-    var offset = 0;
-    if (req.params && req.params['offset']) {
-        offset = parseInt(req.params['offset']);
-    }
+router.get('/duanzi/', function(req, res, next) {
+    // var offset = 0;
+    // if (req.params && req.params['offset']) {
+    //     offset = parseInt(req.params['offset']);
+    // }
 
     jandan
-    .find({})
+    // .find({})
     // .sort({"duanziId": 1})
-    .skip(offset)
-    .limit(20)
+    // .skip(offset)
+    // .limit(20)
+    .aggregate({$sample: {size: 20}})
     .exec((err, data) => {
         if (err) {
             throw err;
